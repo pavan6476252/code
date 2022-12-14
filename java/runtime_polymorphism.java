85325e066811737e8660c47b05631577e9fb88e2
Pavan_Kumar <63488643+pavan6476252@users.noreply.github.com>
2022-12-14 14:36:49 +0530

    Create runtime_polymorphism.java

---
+++ java/runtime_polymorphism.java
@@ -0,0 +1,26 @@
+
+class Vehicle{
+    void start(){
+        System.out.println("started.............");
+    }
+}
+class Mustang extends Vehicle{
+    void start(){
+        System.out.println("Wroom Wroom .........");
+    }
+}
+class Tesla extends Vehicle{
+    void start(){
+        System.out.println("silent ...... start");
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		Mustang m = new Mustang();
+		Tesla t = new Tesla();
+		t.start();
+		m.start();
+	}
+}
