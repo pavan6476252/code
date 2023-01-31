cd4ec44afaa67c1c1d230b81cb317af102983689
Pavan_Kumar <63488643+pavan6476252@users.noreply.github.com>
2023-01-31 14:40:38 +0000

    ok

--- data_structure/lab_pracatice/infix_to_post_fix.c
+++ data_structure/lab_pracatice/infix_to_post_fix.c
@@ -1,53 +1,69 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 char stack[100];
-int top=-1;
-void push(char n){
-    
-    stack[++top]=n;
+int top = -1;
+void push(char n)
+{
+
+    stack[++top] = n;
 }
-char pop(){
-    if(top==-1)return -1;
+char pop()
+{
+    if (top == -1)
+        return -1;
     else
-    return stack[top--];
+        return stack[top--];
 }
 
-int priority(char a){
-    if(a=='(') return 0;
-    if(a=='+'|| a=='-') return 1;
-    if(a=='*'|| a=='/') return 2;
-
+int priority(char a)
+{
+    if (a == '(')
+        return 0;
+    if (a == '+' || a == '-')
+        return 1;
+    if (a == '*' || a == '/')
+        return 2;
 }
-void main(){
+void main()
+{
     char str[20];
-    char a,*x;
-    scanf("%s",str);
-    x=str;
+    char a, *e;
+    scanf("%s", str);
+    e = str;
 
-    while(*x!= '\0'){
-       
-        if(*x>='0'&&*x<='9'){
-            printf("%c",*x);
-        }else if(*x == '('){
-            push(*x);
-        }else if(*x==')'){
-            while((a=pop())!=')'){
-                printf("%c",a);
+    while (*e != '\0')
+    {
+
+        if (*e >= '0' && *e <= '9')
+        {
+            printf("%c", *e);
+        }
+        else if (*e == '(')
+        {
+            push(*e);
+        }
+        else if (*e == ')')
+        {
+
+            while ((a = pop()) != '(')
+            {
+                printf("%c", a);
             }
-            
-        // }else{
-        //     while(priority(stack[top]) >= priority(*x)){
-        //         printf("%c",pop());
+        
+        }else{
+            while(priority(stack[top]) >= priority(*e)){
+                printf("%c",pop());
 
-        //     }
-        //     push(*x);
-        // }
+            }
+            push(*e);
+        }
 
-        x++;
+        e++;
     }
-    while(top!=-1){
-        printf("%c",pop());
+    while (top != -1)
+    {
+        printf("%c", pop());
     }
 }
