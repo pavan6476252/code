af18a455f00f675ac47f808f1e722e5bfc0668a0
Pavan_Kumar <63488643+pavan6476252@users.noreply.github.com>
2023-02-02 15:59:48 +0000

    binary_finish

---
+++ java/binary_search.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.Scanner;
+class binary_search{
+    
+    public static void main(String args []){
+     Scanner sc = new  Scanner(System.in);
+     int n = sc.nextInt();
+     int arr[] = new int[n];
+     for(int i=0;i<n;i++){
+        arr[i] = sc.nextInt();
+     }
+
+     int mid,i=0;
+     while(i<n){
+        mid = (i+n)/2;
+        if(arr[i]==key)
+     {
+        isFound =1;
+        break;
+     }
+     if(arr[mid]> key){
+
+     }
+     }
+    }
+}
