fac07d3ccf4dc7b3e28f237ba467acf4ff1fb31d
Pavan_Kumar <63488643+pavan6476252@users.noreply.github.com>
2023-02-02 16:00:11 +0000

    binary

---
+++ java/primitive_values.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+class primitive_values{
+       static int integer;
+       static float floating;
+      static  double double_val;
+      static  char character;
+       static long longNumber;
+       static boolean bool;
+        static short st;
+       static byte byteValue;
+    public static void main(String args []){
+    System.out.println("integer "+ integer);
+    System.out.println("float "+ floating);
+    System.out.println("double "+ double_val);
+    System.out.println("long "+ longNumber);
+    System.out.println("bool "+ bool);
+    System.out.println("short "+ st);
+    System.out.println("byte "+ byteValue);
+    System.out.println("char "+ character);
+
+    }
+}
